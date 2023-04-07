87871463a1801233f2aba6768d261193b4aa5429
nasim <nasim@10.0.0.10>
2023-04-07 18:56:59 -0400

    Initial part

---
+++ src/day2Demo/Day2Class.java
@@ -0,0 +1,15 @@
+package day2Demo;
+
+public class Day2Class {
+	
+	public static void main(String[] args) {
+		 
+		
+		System.out.println("test class");
+		
+		System.out.println("class project");
+	}
+		
+	
+
+}
