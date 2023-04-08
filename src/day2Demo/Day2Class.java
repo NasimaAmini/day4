6af083ae24b98fb1e931a31781dd73edddc2eaf8
nasim <nasim@10.0.0.10>
2023-04-07 20:29:10 -0400

    implemented getinfo()

--- src/day2Demo/Day2Class.java
+++ src/day2Demo/Day2Class.java
@@ -3,13 +3,10 @@ package day2Demo;
 public class Day2Class {
 	
 	public static void main(String[] args) {
-		 
 		
-		System.out.println("test class");
-		
-		System.out.println("class project");
 	}
 		
-	
-
+		public void getInfo() {
+			System.out.println("Pushing code...");
+		}
 }
